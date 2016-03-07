a7ab9a19593232cf35780995c3bc5b33c826ba85
tangju0418 <635465547@qq.com>
2016-03-07 23:03:33 +0800

    图片处理的测试更新，Ajax的测试

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,8 +1,50 @@
 /**
  * Created by Administrator on 2016/3/6.
  */
-function showPic(whichpic){
-    var source= whichpic.getAttribute("href");
+function showPic(whichPic){
+    var source= whichPic.getAttribute("href");
     var placeholder=document.getElementById("display");
     placeholder.setAttribute("src",source);
+    var text=whichPic.getAttribute("title");
+    var description=document.getElementById("description");
+    description.firstChild.nodeValue=text;
 }
+//function countBodyChildren(){
+//    var body_element=document.getElementsByTagName("body")[0];
+
+   // alert(body_element.childNodes.length);
+   // alert(body_element.nodeType);
+//}
+
+//window.onload=countBodyChildren;
+//window.onload=prepareLinks;
+//function prepareLinks(){
+//    var links=document.getElementsByTagName("a");
+//    for(var i=0;i<links.length;i++){
+//        if(links[i].getAttribute("class")=="popup"){
+//            links[i].onclick=function(){
+//                popUp(this.getAttribute("href"));
+//                return false;
+//            }
+//        }
+//    }
+//}
+//function popUp(winURL){
+//    window.open(winURL,"popup","width=320,height=480");
+//}
+
+
+function prepareGallery(){
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imageGallery")) return false;
+    var gallery=document.getElementById("imageGallery");
+    var links=gallery.getElementsByTagName("a");
+    for(var i=0;i<links.length;i++){
+        links[i].onclick=function(){
+            showPic(this);
+            return false;
+        }
+    }
+}
+window.onload=prepareGallery;
