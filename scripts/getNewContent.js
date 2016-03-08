f685186f6f0c92679606547d1b5560dcb2f4e4cc
tangju0418 <635465547@qq.com>
2016-03-08 22:28:56 +0800

    动画效果测试

--- scripts/getNewContent.js
+++ scripts/getNewContent.js
@@ -3,10 +3,16 @@ function getNewContent(){
     if(request){
         request.open("GET","example.txt",true);
         request.onreadystatechange= function( ) {
+
             if(request.readyState==4){
+
                 var para=document.createElement("p");
                 var txt=document.createTextNode(request.responseText);
+                alert(request.responseText);
+
                 para.appendChild(txt);
+                alert(document.getElementById("new"));
+
                 document.getElementById('new').appendChild(para);
             }
         };
@@ -15,6 +21,6 @@ function getNewContent(){
     else{
         alert('Sorry,your browser doesn\'t support XMLHttpRequest');
     }
-    alert("Function Done");
+
 }
 addLoadEvent(getNewContent);
